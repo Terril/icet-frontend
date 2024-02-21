204466a514297b3ec836828b12fb87dcefa9185c
Terril Thomas <terril_palliparambil@flyscoot.com>
2024-02-21 15:41:57 +0800

    UI for signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:icet/const/colors.dart';
 import 'package:icet/signupsignin/signup.dart';
 import 'icetbinding.dart';
 import 'overviewboard/overviewboard.dart';
 
 void main() {
   runApp(GetMaterialApp(
+    theme: ThemeData(scaffoldBackgroundColor: colorBlue),
     initialRoute: '/signup',
     getPages: [
       GetPage(name: '/overviewboard', page: () => OverviewboardView(), binding: IceTBinding()),
--- lib/signupsignin/signup.dart
+++ lib/signupsignin/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:get/utils.dart';
 import 'package:icet/signupsignin/signup_controller.dart';
 
 import '../const/colors.dart';
@@ -8,31 +10,169 @@ import '../const/colors.dart';
 class SignupView extends GetView<SignupController> {
   SignupView({super.key});
 
+  void _submitCreateAccount() {
+    print("Create account button clicked");
+  }
+
   @override
   Widget build(context) {
+    double txtWidth = Get.width * 0.3;
     return Scaffold(
-      body: Center(child: Card(
-          child: Padding(padding: const EdgeInsets.all(32.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-            const Text("Sign up",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: colorGrey,
-                      fontSize: 20)),
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              child: const Text("Max MagiX,",
-                  style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                      fontSize: 16)),
-            )
-          ]))),
-    ));
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo.png"),
+            const SizedBox(width: 15),
+            const Text(
+              'Ice T',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          child: const Text("Decide Better",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500, color: colorGrey, fontSize: 20)),
+        ),
+        Container(
+            margin: const EdgeInsets.only(top: 40),
+            child: Card(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Text("Sign up",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: colorGrey,
+                                  fontSize: 20)),
+                          Container(
+                            margin: const EdgeInsets.only(top: 24),
+                            child: const Text("Your email",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: colorGrey,
+                                    fontSize: 14)),
+                          ),
+                          Container(
+                              width: txtWidth,
+                              margin: const EdgeInsets.only(top: 8),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: colorGreyTextFiled),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    hintText: 'name@example.com'),
+                              )),
+                          Container(
+                            margin: const EdgeInsets.only(top: 20),
+                            child: const Text("Password",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: colorGrey,
+                                    fontSize: 14)),
+                          ),
+                          Container(
+                              width: txtWidth,
+                              margin: const EdgeInsets.only(top: 8),
+                              child: TextField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: colorGreyTextFiled),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    hintText: '********'),
+                              )),
+                          Container(
+                            margin: const EdgeInsets.only(top: 20),
+                            child: const Text("Re-input password",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: colorGrey,
+                                    fontSize: 14)),
+                          ),
+                          Container(
+                              width: txtWidth,
+                              margin: const EdgeInsets.only(top: 8),
+                              child: TextField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: colorGreyTextFiled),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    hintText: '********'),
+                              )),
+                          Container(
+                              width: txtWidth,
+                              margin: const EdgeInsets.only(top: 24.0),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                          8), // <-- Radius
+                                    ),
+                                    backgroundColor: colorBlueButton),
+                                onPressed: _submitCreateAccount,
+                                child: const Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text('Create account',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: colorWhite)),
+                                ),
+                              )),
+                          Container(
+                              margin: const EdgeInsets.only(top: 16),
+                              child: RichText(
+                                textAlign: TextAlign.left,
+                                text: const TextSpan(
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: 'Already registered?',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            color: colorGrey,
+                                            fontSize: 14)),
+                                    TextSpan(
+                                        text: ' Sign in',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            color: colorBlueButton,
+                                            fontSize: 14)),
+                                  ],
+                                ),
+                              )),
+                        ]))))
+      ],
+    )));
   }
 }
