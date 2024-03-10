0432ab7c41c6c6077f6cbbe00d6c27dbc757c10a
Terril Thomas <terril_palliparambil@flyscoot.com>
2024-03-11 02:28:34 +0800

    Start working on wysiwyg

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -7,5 +7,6 @@ const colorWhite = Color(0xFFFFFFFF);
 const colorBlue = Color(0xffEBF5FF);
 const colorBlueButton = Color(0xFF1A56DB);
 const colorBlue100 = Color(0xffE1EFFE);
+const colorGreyEditor = Color(0xfff9fafb);
 
 
--- lib/overviewboard/assets/assets.dart
+++ lib/overviewboard/assets/assets.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:icet/const/colors.dart';
 
 import '../overviewboard_controller.dart';
 
@@ -14,7 +16,6 @@ class AssetsView extends GetView<OverviewboardController> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.5),
       // this is the main reason of transparency at next screen. I am ignoring rest implementation but what i have achieved is you can see.
@@ -23,7 +24,7 @@ class AssetsView extends GetView<OverviewboardController> {
             alignment: FractionalOffset.topRight,
             widthFactor: 0.5,
             child: Container(
-              padding: const EdgeInsets.all(16),
+                padding: const EdgeInsets.all(16),
                 color: Colors.white,
                 child: Column(children: <Widget>[
                   Align(
@@ -36,38 +37,95 @@ class AssetsView extends GetView<OverviewboardController> {
                     ),
                   ),
                   const SizedBox(height: 16),
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                    const Text(
-                      'New Asset',
-                      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
-                    ),
-                    IconButton(
-                      color: Colors.red,
-                      icon: const Icon(Icons.delete_outlined),
-                      onPressed: () {
-                        Get.back();
-                      },
-                    )
-                  ]),
-                  QuillToolbar.simple(
-                    configurations: QuillSimpleToolbarConfigurations(
-                      controller: _controller,
-                      sharedConfigurations: const QuillSharedConfigurations(
-                        locale: Locale('de'),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: QuillEditor.basic(
-                      configurations: QuillEditorConfigurations(
-                        controller: _controller,
-                        readOnly: false,
-                        sharedConfigurations: const QuillSharedConfigurations(
-                          locale: Locale('en'),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'New Asset',
+                          style: TextStyle(
+                              fontSize: 24.0, fontWeight: FontWeight.w600),
                         ),
-                      ),
-                    ),
-                  )
+                        IconButton(
+                          color: Colors.red,
+                          icon: const Icon(Icons.delete_outlined),
+                          onPressed: () {
+                            Get.back();
+                          },
+                        )
+                      ]),
+                  const SizedBox(height: 24),
+                  SizedBox(
+                      height: Get.height / 2.75,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: colorGreyField)),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  color: colorGreyEditor,
+                                  width: Get.width,
+                                  child: QuillToolbar.simple(
+                                    configurations:
+                                        QuillSimpleToolbarConfigurations(
+                                      buttonOptions:
+                                          const QuillSimpleToolbarButtonOptions(
+                                              base:
+                                                  QuillToolbarBaseButtonOptions(
+                                                      iconTheme: QuillIconTheme(
+                                        iconButtonSelectedData: IconButtonData(
+                                            highlightColor: colorBlue),
+                                        iconButtonSelectedStyle: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStatePropertyAll<Color>(
+                                                    colorBlue)),
+                                      ))),
+                                      dialogTheme: const QuillDialogTheme(
+                                          buttonStyle: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStatePropertyAll<
+                                                      Color>(colorBlue)),
+                                          dialogBackgroundColor:
+                                              colorGreyEditor),
+                                      toolbarIconAlignment: WrapAlignment.start,
+                                      toolbarIconCrossAlignment:
+                                          WrapCrossAlignment.start,
+                                      showDividers: true,
+                                      showFontFamily: false,
+                                      showFontSize: false,
+                                      showStrikeThrough: false,
+                                      showInlineCode: false,
+                                      showSubscript: false,
+                                      showSuperscript: false,
+                                      showBackgroundColorButton: false,
+                                      showClearFormat: false,
+                                      showCodeBlock: false,
+                                      showQuote: false,
+                                      showIndent: false,
+                                      showUndo: false,
+                                      showRedo: false,
+                                      showSearchButton: false,
+                                      sectionDividerColor: Colors.black,
+                                      controller: _controller,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: QuillEditor.basic(
+                                    configurations: QuillEditorConfigurations(
+                                      padding: EdgeInsets.all(5),
+                                      controller: _controller,
+                                      readOnly: false,
+                                      sharedConfigurations:
+                                          const QuillSharedConfigurations(
+                                        locale: Locale('en'),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ]))),
                 ]))),
       ),
     );
