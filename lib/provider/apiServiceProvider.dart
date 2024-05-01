7b331401cc2eea75809af6fdbc27be84d7f22d65
Terril Thomas <terril_palliparambil@flyscoot.com>
2024-05-01 15:20:52 +0800

    Update patch issue

--- lib/provider/apiServiceProvider.dart
+++ lib/provider/apiServiceProvider.dart
@@ -18,28 +18,51 @@ class APIServiceProvider extends GetConnect with CacheManager {
 
   String _getUrl() => _baseUrl;
 
+  @override
+  Future<Response<T>> patch<T>(
+    String url,
+    dynamic body, {
+    String? contentType,
+    Map<String, String>? headers,
+    Map<String, dynamic>? query,
+    Decoder<T>? decoder,
+    Progress? uploadProgress,
+  }) {
+    return super.request<T>(
+      url,
+      'PATCH',
+      body: body,
+      headers: headers,
+      contentType: contentType,
+      query: query,
+      decoder: decoder,
+      uploadProgress: uploadProgress,
+    );
+  }
+
   // Get request
-   Future<Response> getBoard() { //List<dynamic>>
+  Future<Response> getBoard() {
+    //List<dynamic>>
     var token = getToken();
     Logger.printLog(message: "$token");
     return get('$_baseUrl/api/boards/', headers: {
-      'Authorization' : 'Token $token',
-      'accept' : 'application/json',
-      'Content-Type' : 'application/json'
+      'Authorization': 'Token $token',
+      'accept': 'application/json',
+      'Content-Type': 'application/json'
     });
   }
 
   Future<Response<List<dynamic>>> addBoard(Map data) {
     var token = getToken();
     return post('$_baseUrl/api/boards/', data, headers: {
-    'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
-  Future<Response> updateBoardNameByBoardId(String boardId , Map map) {
+  Future<Response> updateBoardNameByBoardId(String boardId, Map map) {
     var token = getToken();
     return patch('$_baseUrl/api/boards/$boardId/', map, headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
@@ -47,48 +70,54 @@ class APIServiceProvider extends GetConnect with CacheManager {
   Future<Response> getApiBoardsCustom() {
     Map<String, dynamic> map = HashMap();
     map["board_name"] = "Custom";
-    map["column_names"] = ["Interest level", "Checklist 1", "Checklist 2", "Checklist 3"];
+    map["column_names"] = [
+      "Interest level",
+      "Checklist 1",
+      "Checklist 2",
+      "Checklist 3"
+    ];
     map["row_names"] = ["Asset 1", "Asset 2", "Asset 3"];
 
     var token = getToken();
     Logger.printLog(message: "$token");
     return post('$_baseUrl/api/boards/custom/', map, headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
   Future<Response> getColumnsByBoardId(String? boardId) {
     var token = getToken();
     return get('$_baseUrl/api/boards/$boardId/columns', headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
   Future<Response> addRows(Map map) {
     var token = getToken();
     return post('$_baseUrl/api/rows/', map, headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
   Future<Response> deleteRow(String rowId) {
     var token = getToken();
     return delete('$_baseUrl/api/row/$rowId/delete', headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
+
   // Column is nothing but the checklist
   Future<Response> addColumn(Map data) {
     var token = getToken();
     return post('$_baseUrl/api/columns/', data, headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
   Future<Response> deleteColumn(String columnId) {
     var token = getToken();
     return delete('$_baseUrl/api/column/$columnId/delete', headers: {
-      'Authorization' : 'Token $token',
+      'Authorization': 'Token $token',
     });
   }
 
@@ -96,7 +125,8 @@ class APIServiceProvider extends GetConnect with CacheManager {
   Future<Response> signupUser(Map data) => post('$_baseUrl/api/users/', data);
 
   // Post Sign in request
-  Future<Response> signinUser(Map data) => post('$_baseUrl/api/api-token-auth/', data);
+  Future<Response> signinUser(Map data) =>
+      post('$_baseUrl/api/api-token-auth/', data);
 
   // Post request with File
 
