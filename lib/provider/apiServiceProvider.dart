da36d6bc1c0f97285a1043efdd678103ed48cee8
Terril <terril.thomas97@gmail.com>
2025-04-16 07:28:16 +0800

    Update apiServiceProvider.dart

--- lib/provider/apiServiceProvider.dart
+++ lib/provider/apiServiceProvider.dart
@@ -14,7 +14,6 @@ import '../logs.dart';
 
 class APIServiceProvider extends GetConnect with CacheManager {
   final String _baseUrl = "http://127.0.0.1:8000";
-  final String _prod_baseUrl = "https://icet-django.fly.dev";
 
 
   void _getUrl() => _baseUrl;
